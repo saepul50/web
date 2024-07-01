b9cebf360675f51f5ee9b57cdb6b1639bdd6bac0
saepul50 <161146248+saepul50@users.noreply.github.com>
2024-07-01 16:48:22 +0700

    Add files via upload

---
+++ portofolio/script.js
@@ -0,0 +1,34 @@
+// navbar responsive
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// navbarnyala
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+// garis navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 600);
+// hilangkan togel selesai klik
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
